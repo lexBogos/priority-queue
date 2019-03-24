b9d3abda9578291e1f4d177258118b7ad8d45973
Lexbogos <lex.bogos@gmail.com>
2019-03-24 22:08:07 +0300

    14 mistakes

--- src/max-heap.js
+++ src/max-heap.js
@@ -7,43 +7,122 @@ class MaxHeap {
 	}
 
 	push(data, priority) {
-		
+		const node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
-	pop() {
-		
+		pop() {
+
+		if(this.root) {
+      const detached = this.detachRoot();
+      if(this.parentNodes.length > 0) {
+      	let newRoot = this.restoreRootFromLastInsertedNode(detached);
+        this.shiftNodeDown(newRoot);
+	    }
+      return detached.data;
+    }
 	}
 
 	detachRoot() {
 		
+		if (this.root == this.parentNodes[0]) {
+			this.parentNodes.shift()
+		}
+		let root_buffer = this.root;
+		this.root = null;
+		return root_buffer;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
+		const last_inserted = this.parentNodes.pop();
+		if (last_inserted){
+			if (last_inserted.parent === detached) {
+				this.parentNodes.unshift(last_inserted);
+			} 
+			else {this.parentNodes.unshift(last_inserted.parent);
+			}
+			last_inserted.remove();
+			this.root = last_inserted;
+
+			if (detached.left !== null) {
+				last_inserted.appendChild(detached.left);
+			}
+			if (detached.right !== null) {
+				last_inserted.appendChild(detached.right);
+			}
+		}
 	}
 
 	size() {
-		
+		let heap_size = 0;
+		function count(node) {
+			if (node === null) {
+				return;
+			}
+			heap_size++;
+			count(node.left);
+			count(node.right);   
+		}
+		count(this.root);
+		return heap_size;
 	}
 
 	isEmpty() {
-		
+		return this.parentNodes.length === 0 ?  true : false
 	}
 
 	clear() {
-		
+
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if (this.root === null) {
+			this.root = node;
+			this.parentNodes.push(node);
+		}
+		else{
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.push(node);
+			if (this.parentNodes[0].left !== null && this.parentNodes[0].right !== null) {
+				this.parentNodes.shift();
+			}
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+		if (node.parent === null) {
+			this.root = node;
+		}
+		else{
+			if (node.priority > node.parent.priority) {
+				const buffer_for_nodes = this.parentNodes.indexOf(node);
+				const buffer_for_nodes_parent = this.parentNodes.indexOf(node.parent);
+				this.parentNodes[buffer_for_nodes] = node.parent;
+				this.parentNodes[buffer_for_nodes_parent] = node;
+				
+				
+				node.swapWithParent();
+				this.shiftNodeUp(node);
+			}
+		}
 	}
 
 	shiftNodeDown(node) {
-		
+		if (node !== null && node.left!==null) {
+			
+			if (node.priority < node.left.priority){
+				
+
+
+				node.left.swapWithParent();
+
+			}
+
+		}
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,36 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(maxSize){
+			this.maxSize = maxSize
+		} else {
+			this.maxSize = 30;
+		}
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
-
+		if(this.size() >= this.maxSize)
+		{throw new Error('Переполнение!');}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
+		if (!this.currentSize) {
+      throw new 'Ошибка';
+    } else {
+      this.currentSize--;
+      return this.heap.pop();
+    }
 
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
